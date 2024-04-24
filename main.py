4bee15386d2b8e961d1add23ae13e63ea5de58f1
WooDean <166214492+WooDean@users.noreply.github.com>
2024-04-24 23:51:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -63,18 +63,18 @@ st.title('테스트결과서 마지막 시트에 캡처 삽입하기')
 with st.container():
     # st.write('이미지를 업로드하면, 가로 최대 설정한 장수까지 배열하여 삽입합니다.')
     st.write('**이미지 크기와 간격을 조절하여 적절하게 사용하세요! (모두 픽셀 단위) **')
-    max_images_per_row = st.number_input('한 행에 배치할 이미지 최대 수', min_value=1, value=6)
-    cell_width = st.number_input('셀 너비 간격 설정: 너비를 크게 잡을수록 이미지 가로 간격이 좁아져요', value=100)
-    cell_height = st.number_input('셀 높이 간격 설정: 높이를 크게 잡을수록 이미지 세로 간격이 좁아져요', value=20)
-    start_cell = st.text_input('이미지를 시작할 셀 주소 (예: A2)', value='A2')
-    image_width = st.number_input('이미지의 가로 크기', min_value=100, value=250)
-    image_height = st.number_input('이미지의 세로 크기', min_value=100, value=500)
+    max_images_per_row = st.number_input('가로에 배치할 이미지 수', min_value=1, value=6)
+    cell_width = st.number_input('이미지 너비 간격: 너비를 크게 잡을수록 이미지 가로 간격이 좁아져요', value=100)
+    cell_height = st.number_input('이미지 높이 간격: 높이를 크게 잡을수록 이미지 세로 간격이 좁아져요', value=20)
+    start_cell = st.text_input('이미지 시작 셀 주소 (예: A2)', value='A2')
+    image_width = st.number_input('이미지 가로 크기', min_value=100, value=250)
+    image_height = st.number_input('이미지 세로 크기', min_value=100, value=500)
 
 # 사용자가 업로드할 엑셀 파일
 uploaded_excel = st.file_uploader("테스트결과서 엑셀 파일을 업로드하세요", type=['xlsx'])
 
 # 이미지 업로드
-uploaded_files = st.file_uploader("이미지 파일을 선택하세요 (여러 개 선택 가능)", accept_multiple_files=True, type=['png', 'jpg', 'jpeg'])
+uploaded_files = st.file_uploader("이미지 파일을 선택하세요(여러 개 선택 가능)", accept_multiple_files=True, type=['png', 'jpg', 'jpeg'])
 
 if uploaded_files and uploaded_excel:
     original_filename = uploaded_excel.name  # 업로드한 파일의 원래 이름
@@ -84,7 +84,7 @@ if uploaded_files and uploaded_excel:
         wb = load_workbook(tmp.name)
     
     # 새로운 시트를 생성하여 이미지를 붙여넣을 준비
-    ws = wb.create_sheet(title='테스트결과서 첨부')
+    ws = wb.create_sheet(title='이미지 캡처')
 
     image_size = (image_width, image_height)  # 사용자가 입력한 크기를 토대로 이미지의 크기 설정
     start_row, start_col = coordinate_to_tuple(start_cell)  # 시작 셀 주소로부터의 행, 열 좌표 추출
